e25b92c1bbdc344dc0112e4dc10a3059eec5ac1f
Bader Albarrak <balbarak@live.com>
2019-04-08 23:44:00 +0300

    add app version

---
+++ src/Uploader/Helpers/WebConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uploader.Helpers
+{
+    public class WebConstants
+    {
+        public const string APP_VERSION = "1.0.1";
+    }
+}
